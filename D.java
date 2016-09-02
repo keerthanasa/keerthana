6954a619a99cac92c78d4f1c4083932131b27391
keerthanasa <sakeerthana67@gmail.com>
2016-09-02 11:16:39 +0530

    Create D.java

---
+++ D.java
@@ -0,0 +1,17 @@
+class D
+{
+public static void main(String args[])
+{
+LinkedHashSet s=new LinkedHashSet();
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+char []a=s.toCharArray();
+int n=a.length;
+int i=0;
+for(i=0;i<n;i++)
+{
+s.add(a[i]);
+}
+System.out.println(s);
+}
+}
